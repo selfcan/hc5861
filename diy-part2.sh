abf52bdf23e1a94c806b04e07431981e841169a7
jie.wu <jie.wu@yiducloud.cn>
2022-06-05 19:13:06 +0800

    diy

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,8 +27,46 @@ cat >> .config <<EOF
 # CONFIG_PACKAGE_luci-app-unblockmusic is not set
 EOF
 
+# 5. Protocols
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci-proto-relay=y
+EOF
+
+#kmod
+cat >> .config <<EOF
+CONFIG_DEFAULT_kmod-sdhci-mt7620=y
+CONFIG_DEFAULT_kmod-usb-ledtrig-usbport=y
+CONFIG_DEFAULT_kmod-usb-ohci=y
+CONFIG_DEFAULT_kmod-usb2=y
+EOF
+
+#
+# USB Support
+#
+cat >> .config <<EOF
+CONFIG_PACKAGE_kmod-usb-core=y
+CONFIG_PACKAGE_kmod-usb-ehci=y
+CONFIG_PACKAGE_kmod-usb-ledtrig-usbport=y
+CONFIG_PACKAGE_kmod-usb-ohci=y
+CONFIG_PACKAGE_kmod-usb-storage=y
+CONFIG_PACKAGE_kmod-usb-storage-extras=y
+CONFIG_PACKAGE_kmod-usb2=y
+EOF
+
+#
+# LuCI
+#
+cat >> .config <<EOF
+CONFIG_PACKAGE_luci=y
+CONFIG_PACKAGE_luci-base=y
+CONFIG_LUCI_LANG_zh-cn=y
+CONFIG_PACKAGE_luci-compat=y
+CONFIG_PACKAGE_luci-mod-admin-full=y
+EOF
+
 #使固件支持tf卡扩展
 cat >> .config <<EOF
+CONFIG_PACKAGE_automount=y
 CONFIG_PACKAGE_kmod-nls-cp437=y
 CONFIG_PACKAGE_kmod-nls-iso8859-1=y
 CONFIG_PACKAGE_kmod-nls-utf8=y
