22c5d54c9845147ee6b8024ac51544dc04842564
jie.wu <jie.wu@yiducloud.cn>
2022-06-06 19:42:58 +0800

    diy

--- diy-part2.sh
+++ diy-part2.sh
@@ -54,8 +54,6 @@ EOF
 # 使固件支持tf卡扩展
 cat >> .config <<EOF
 CONFIG_PACKAGE_automount=y
-CONFIG_PACKAGE_kmod-fs-antfs=y
-CONFIG_PACKAGE_antfs-mount=y
 CONFIG_PACKAGE_kmod-nls-cp437=y
 CONFIG_PACKAGE_kmod-nls-iso8859-1=y
 CONFIG_PACKAGE_kmod-nls-utf8=y
@@ -69,12 +67,9 @@ EOF
 
 # Disc
 cat >> .config <<EOF
-CONFIG_PACKAGE_blkdiscard=y
-CONFIG_PACKAGE_blkid=y
 CONFIG_PACKAGE_blockdev=y
 CONFIG_PACKAGE_cfdisk=y
 CONFIG_PACKAGE_cgdisk=y
-CONFIG_PACKAGE_eject=y
 CONFIG_PACKAGE_fdisk=y
 CONFIG_PACKAGE_findfs=y
 CONFIG_PACKAGE_fixparts=y
@@ -82,21 +77,14 @@ CONFIG_PACKAGE_gdisk=y
 CONFIG_PACKAGE_hd-idle=y
 CONFIG_PACKAGE_hdparm=y
 CONFIG_PACKAGE_lsblk=y
-CONFIG_PACKAGE_mdadm=y
 CONFIG_PACKAGE_parted=y
 CONFIG_PACKAGE_partx-utils=y
-CONFIG_PACKAGE_sfdisk=y
-CONFIG_PACKAGE_sgdisk=y
-CONFIG_PACKAGE_wipefs=y
 EOF
 
 # Filesystem
 cat >> .config <<EOF
 CONFIG_PACKAGE_exfat-fsck=y
 CONFIG_PACKAGE_exfat-mkfs=y
-CONFIG_PACKAGE_ntfs-3g=y
-CONFIG_PACKAGE_ntfs-3g-low=y
-CONFIG_PACKAGE_ntfs-3g-utils=y
 CONFIG_PACKAGE_resize2fs=y
 CONFIG_PACKAGE_sysfsutils=y
 EOF
